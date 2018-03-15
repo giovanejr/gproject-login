aba1aea96432914d93aa0318c2390c14298d7acd
br94043 <giovanejunior@natura.net>
2018-03-14 22:02:10 -0300

    access_denied

---
+++ login/access_denied.php
@@ -0,0 +1,14 @@
+<?php
+header('X-PHP-Response-Code: 401', true, 401);
+?>
+
+<html>
+<head>
+<title>Permission Denied</title>
+<meta http-equiv="refresh" content="2; http://188.84.224.35.bc.googleusercontent.com">
+<meta name="keywords" content="automatic redirection">
+</head>
+<body>
+<center><img src="access_denied.jpg"></center>
+</body>
+</html>
